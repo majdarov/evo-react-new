e0cfa95caf7376cb182045094a87453bb7caf169
Mickhael <majdarov@gmail.com>
2024-12-11 20:59:32 +0400

    Modified Button.tsx

--- src/components/common/Forms/Button/Button.tsx
+++ src/components/common/Forms/Button/Button.tsx
@@ -13,13 +13,9 @@ export function Button({ type, label, icon, callback }: IButtonProps) {
     return (
         <button type={type}>
             <div className={s['filter-button']}>
-                {/* <input type={type} value={label} className={s.hidden} /> */}
-                {/* <span className={s.filter} onClick={(e) => callback()}> */}
                 <span className={s.filter} onClick={callback}>
                     {label}
                     <i className={icon}></i>
-                    {/* {type === 'submit' && <button type="submit" value="Submit" className={s.hidden} />}
-                    {type === 'reset' && <button type="reset" value="Reset" className={s.hidden} />} */}
                 </span>
             </div>
         </button>
